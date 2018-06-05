21f26737ab2117a218e3211ff66db0cb7b61ee1b
ouyangziling1990 <wujingwei1990@126.com>
2018-06-05 19:25:47 +0800

    add driectory

---
+++ cralwer/hello_crawler.py
@@ -0,0 +1,18 @@
+import requests
+
+def getHtmlText(url):
+    try:
+        r = requests.get(url, timeout=30)
+        r.raise_for_status()
+        r.encoding = r.apparent_encoding
+        return r.text
+    except:
+        return ""
+
+def main():
+    url = "http://www.baidu.com"
+    r = getHtmlText(url)
+    print(r)
+
+if __name__ == "__main__":
+    main()
