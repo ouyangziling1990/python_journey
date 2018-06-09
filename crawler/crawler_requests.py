7f4f4bd923d0947572825d0996acef171df78778
ouyangziling1990 <wujingwei1990@126.com>
2018-06-09 23:14:12 +0800

    modif reques

--- crawler/crawler_requests.py
+++ crawler/crawler_requests.py
@@ -7,8 +7,8 @@ simple code for HTTP Get method using python requests
 import requests
 def getHtmlText(url):
     try:
-        r = requests.get(url, timeout=30)
-        r.raise_for_status()
+        r = requests.get(url, timeout=100)
+        r.raise_for_status(),
         # print("encoding", r.apparent_encoding)
         r.encoding = 'utf-8'
         return r.text
